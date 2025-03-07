9f9f73f1402701ce27a4e6b27e4cb78ef1bb68ee
hassanahmed987 <hassanahmedsiddqui@gmail.com>
2025-03-07 18:01:55 +0500

    Create TASK 1.cpp

---
+++ LAB 6/TASK 1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Base class: Employee
+class Employee {
+protected:
+    string name;
+    float salary;
+
+public:
+    Employee(string empName, float empSalary) {
+        name = empName;
+        salary = empSalary;
+    }
+
+    void displayDetails() {
+        cout << "Employee Name: " << name << endl;
+        cout << "Salary: $" << salary << endl;
+    }
+};
+
+class Manager : public Employee {
+private:
+    float bonus;
+
+public:
+    Manager(string empName, float empSalary, float empBonus) 
+        : Employee(empName, empSalary) {  
+        bonus = empBonus;
+    }
+
+    void displayDetails() {
+        Employee::displayDetails();  
+        cout << "Bonus: $" << bonus << endl;
+    }
+};
+
+int main() {
+    string name;
+    float salary, bonus;
+
+    cout << "Enter Manager Name: ";
+    getline(cin, name);
+    cout << "Enter Salary: $";
+    cin >> salary;
+    cout << "Enter Bonus: $";
+    cin >> bonus;
+
+    Manager mgr(name, salary, bonus);
+
+    cout << "\nManager Details:\n";
+    mgr.displayDetails();
+
+    return 0;
+}
+
