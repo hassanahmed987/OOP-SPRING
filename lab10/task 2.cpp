b2cf9c169277da24f3de1b03474b9980a0662e5f
hassanahmed987 <hassanahmedsiddqui@gmail.com>
2025-05-03 15:07:17 +0500

    Create task 2.cpp

---
+++ lab10/task 2.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
+using namespace std;
+
+int main() {
+    ifstream in("vehicles.txt");
+
+    if (!in) {
+        cout << "ERROR: Could not open vehicles.txt file" << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(in, line)) {
+        if (line.empty() || line.substr(0, 2) == "//")
+            continue; 
+       istringstream ss(line);
+       string type,id, name, year, extraData, certification;
+       getline(ss,type,',');
+       getline(ss,id,',');
+       getline(ss,name,',');
+       getline(ss,year,',');
+       getline(ss,extraData,',');
+       getline(ss,certification,',');
+        cout << "Type: " << type << endl;
+        cout << "ID: " << id << endl;
+        cout << "Name: " << name << endl;
+        cout << "Year: " << year << endl;
+        cout << "ExtraData: " << extraData << endl;
+        cout << "Certification: " << certification << endl;
+
+    }
+
+    in.close();
+    return 0;
+}
