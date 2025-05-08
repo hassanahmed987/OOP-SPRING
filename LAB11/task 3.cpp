91af51ce0d0e1b828e6c0b444b34507df6b05575
hassanahmed987 <hassanahmedsiddqui@gmail.com>
2025-05-09 00:14:17 +0500

    Update task 3.cpp

--- LAB11/task 3.cpp
+++ LAB11/task 3.cpp
@@ -1,1 +1,39 @@
+#include<iostream>
+#include <stdexcept>
+using namespace std;
 
+
+class TemperatureException:public exception{
+
+
+};
+
+template<typename T>
+class Temperature{
+private:
+ T celcius;
+ public:
+ Temperature(T celcius):celcius(celcius){}
+
+ void convert(){
+    if(celcius< (-273.15)){
+        throw TemperatureException();
+    }
+    T farhen=(celcius*1.8)+32;
+    cout<<"Farheniet Value is "<<farhen<<endl;
+ }
+};
+
+int main(){
+    try{
+        cout<<"Attempting to Conver 12"<<endl;
+        Temperature<int> T1(12);
+        T1.convert();
+        cout<<"Attempting to Conver -280"<<endl;
+        Temperature<double> T2(-280);
+        T2.convert();
+    }catch(TemperatureException &e){
+        cout<<"Caught Exception: "<<e.what()<<endl;
+    }
+    return 0;
+}
